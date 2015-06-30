045edef7eb88fd4db27517889b99f14ffba08c9c
Dennis Drenner <dennis@dennisdrenner.com>
2015-06-30 12:19:11 -0400

    Made numerous small changes after code review

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,74 +1,75 @@
 
 
-//use strict";
+"use strict";
+
 // JSON OBJECTS WITH BIO AND OTHER INFO FOR RESUME
 
 var bio = {
   "name": "Dennis A. Drenner",
   "role": "Web Developer",
-  "pictureURl" : "https://pbs.twimg.com/profile_images/450269902050312192/SQuOpWgy.jpeg",
-  "welcomeMessage" : "Custom web design since 1845.",
-  "skills" : ["web developer", "photographer", "paraglider pilot", "chef"],
-  "contacts" : {
-    "mobile" :"301-437-6972",
-    "email" : "dennis@dennisdrenner.com",
-    "github" : "dennisdrenner",
-    "twitter" : "@DennisDrenner",
-    "location" : "Baltimore, MD"
-  }
+  "pictureURl": "https://pbs.twimg.com/profile_images/450269902050312192/SQuOpWgy.jpeg",
+  "welcomeMessage": "Custom web design since 1845.",
+  "skills": ["web developer", "photographer", "paraglider pilot", "chef"],
+  "contacts": {
+    "mobile": "301-437-6972",
+    "email": "dennis@dennisdrenner.com",
+    "github": "dennisdrenner",
+    "twitter": "@DennisDrenner",
+    "location": "Baltimore, MD"
+    }
 };
 
 var work = {
-  "jobs" : [
+  "jobs": [
   {
-    "employer" : "Self (Dennis Drenner Photographs)",
-    "title" : "Principal",
-    "location" : "Baltimore, MD",
-    "dates" : "1991-Present",
-    "description" : "Self-employed photojournalist and wedding photographer. Specialize in photographing people" +
+    "employer": "Self (Dennis Drenner Photographs)",
+    "title": "Principal",
+    "location": "Baltimore, MD",
+    "dates": "1991-Present",
+    "description": "Self-employed photojournalist and wedding photographer. Specialize in photographing people" +
     ", both portraiture and candid event photography."},
 
   {
-    "employer" : "Washington Post",
-    "title" : "Freelance Photographer",
-    "location" : "Washington, DC",
-    "dates" : "1995-2005",
-    "description" : "Regular contributor to the Washington Post Metro, Style and Sports sections."
+    "employer": "Washington Post",
+    "title": "Freelance Photographer",
+    "location": "Washington, DC",
+    "dates": "1995-2005",
+    "description": "Regular contributor to the Washington Post Metro, Style and Sports sections."
   },
 
   {
-    "employer" : "Baltimore Jewish Times",
-    "title" : "Director of Photography",
-    "location" : "Baltimore, MD",
-    "dates" : "2005-2007",
-    "description" : "Chief photographer and DP for small Baltimore Jewish weekly newspaper."}
+    "employer": "Baltimore Jewish Times",
+    "title": "Director of Photography",
+    "location": "Baltimore, MD",
+    "dates": "2005-2007",
+    "description": "Chief photographer and DP for small Baltimore Jewish weekly newspaper."}
   ]
 };
 
 var projects = {
-  "projects" : [
+  "projects": [
   {
-    "title" : "Hampden Alley Portraits",
-    "location" : "Baltimore, MD",
-    "dates" : "2014-Present",
-    "description" : "Street portraits of people from the alley behind my house",
-    "images" : ["http://www.ddcodes.com/static/assets/images/art/hampden-alley-ports-screen-shot.jpg"]
+    "title": "Hampden Alley Portraits",
+    "location": "Baltimore, MD",
+    "dates": "2014-Present",
+    "description": "Street portraits of people from the alley behind my house",
+    "images": ["http://www.ddcodes.com/static/assets/images/art/hampden-alley-ports-screen-shot.jpg"]
   },
 
   {
-    "title" : "Referral Puppy",
-    "location" : "Baltimore, MD",
-    "dates" : "2014",
-    "description" : "Referral tracking web appplication",
-    "images" : ["http://www.ddcodes.com/static/assets/images/art/referral-puppy-screen-shot.png"]
+    "title": "Referral Puppy",
+    "location": "Baltimore, MD",
+    "dates": "2014",
+    "description": "Referral tracking web appplication",
+    "images": ["http://www.ddcodes.com/static/assets/images/art/referral-puppy-screen-shot.png"]
   },
 
   {
-    "title" : "Hampden Alley Portraits",
-    "location" : "Baltimore, MD",
-    "dates" : "2014-Present",
-    "description" : "Street portraits of people from the alley behind my house",
-    "images" : ["http://www.ddcodes.com/static/assets/images/art/hampden-alley-ports-screen-shot.jpg"]
+    "title": "Hampden Alley Portraits",
+    "location": "Baltimore, MD",
+    "dates": "2014-Present",
+    "description": "Street portraits of people from the alley behind my house",
+    "images": ["http://www.ddcodes.com/static/assets/images/art/hampden-alley-ports-screen-shot.jpg"]
   }
   ]
 };
@@ -77,38 +78,38 @@ var projects = {
 var education = {
   "schools": [
     {
-    "name" : "University of Maryland",
-    "location" :  "College Park, MD",
-    "degree" : "BS",
-    "majors" : ["Biology", "German"],
-    "dates" : "1987-1992",
-    "url" : "http://www.umd.edu/"
+    "name": "University of Maryland",
+    "location":  "College Park, MD",
+    "degree": "BS",
+    "majors": ["Biology", "German"],
+    "dates": "1987-1992",
+    "url": "http://www.umd.edu/"
      },
 
 
      {
-    "name" : "University of Mannheim",
-    "location" : "Mannheim, Germany",
-    "degree" : "Certificate of Proficiency in German",
-    "majors" : ["German","Beer Drinking"],
-    "dates" : "1991",
-    "url" : "https://www.uni-mannheim.de/1/"
+    "name": "University of Mannheim",
+    "location": "Mannheim, Germany",
+    "degree": "Certificate of Proficiency in German",
+    "majors": ["German","Beer Drinking"],
+    "dates": "1991",
+    "url": "https://www.uni-mannheim.de/1/"
      }
   ],
 
   onlineCourses: [
      {
-    "name" : "Introduction to Computer Programming",
-    "site" : "Udacity",
-    "dates" : "2013",
-    "url" : "www.udacity.com"
+    "name": "Introduction to Computer Programming",
+    "site": "Udacity",
+    "dates": "2013",
+    "url": "www.udacity.com"
      },
 
      {
-    "name" : "AJAX",
-    "site" : "Udacity",
-    "dates" : "2014",
-    "url" : "www.udacity.com"
+    "name": "AJAX",
+    "site": "Udacity",
+    "dates": "2014",
+    "url": "www.udacity.com"
      }
   ]
 };
@@ -117,8 +118,9 @@ var education = {
 
 // ADD NAME AND ROLE TO TOP OF PAGE
 
-var HTMLheaderName = HTMLheaderName.replace('%data%', bio.name);
-var HTMLheaderRole = HTMLheaderRole.replace('%data%', bio.role);
+HTMLheaderName = HTMLheaderName.replace('%data%', bio.name);
+HTMLheaderRole = HTMLheaderRole.replace('%data%', bio.role);
+
 
 $('#header').prepend(HTMLheaderRole);
 $('#header').prepend(HTMLheaderName);
@@ -127,7 +129,8 @@ $('#header').prepend(HTMLheaderName);
 // ADD CONTACT STRIP TO PAGE
 
 bio.contactStrip = function (bio) {
-  for (item in bio.contacts) {
+ var item;
+ for (item in bio.contacts) {
     var contact = HTMLcontactGeneric.replace('%contact%', item).replace('%data%', bio.contacts[item]);
     $('#topContactStrip').append(contact);
     $('#footerContacts').append(contact);
@@ -138,16 +141,17 @@ bio.contactStrip(bio);
 
 // BUILD TOP SUMMARY SECTION
 
-var HTMLbioPic = HTMLbioPic.replace('%data%', bio.pictureURl);
-var HTMLWelcomeMsg = HTMLWelcomeMsg.replace('%data%', bio.welcomeMessage);
+HTMLbioPic = HTMLbioPic.replace('%data%', bio.pictureURl);
+HTMLWelcomeMsg = HTMLWelcomeMsg.replace('%data%', bio.welcomeMessage);
 
 
 bio.summaryText = function(bio) {
     $('#topSummaryText').append(HTMLWelcomeMsg);
     if (bio.skills.length > 0) {
       $('#topSummaryText').append(HTMLskillsStart);
+      var i;
       for (i in bio.skills) {
-      skill = HTMLskills.replace('%data%', bio.skills[i]);
+      var skill = HTMLskills.replace('%data%', bio.skills[i]);
       $('#skills').append(skill);
       }
     }
@@ -161,6 +165,7 @@ bio.summaryText(bio);
 // BUILD WORK EXPERIENCE SECTION
 
 work.workExperienceBuilder = function (work_obj) {
+  var job;
   for (job in work_obj.jobs) {
     $('#workExperience').append(HTMLworkStart);
     var employer = HTMLworkEmployer.replace('%data%', work_obj.jobs[job].employer);
@@ -179,6 +184,7 @@ work.workExperienceBuilder(work);
 // BUILD PROJECTS SECTION
 
 projects.projectSectionBuilder = function (projects_obj) {
+  var item;
   for (item in projects_obj.projects) {
     $('#projects').append(HTMLprojectStart);
     var title = HTMLprojectTitle.replace('%data%', projects_obj.projects[item].title);
@@ -197,6 +203,7 @@ projects.projectSectionBuilder(projects);
 // BUILD EDUCATION SECTION
 
 education.educationSectionBuilder = function (education_obj) {
+  var item;
   for (item in education_obj.schools) {
   $('#education').append(HTMLschoolStart);
   var name = HTMLschoolName.replace('%data%', education_obj.schools[item].name);
@@ -205,6 +212,7 @@ education.educationSectionBuilder = function (education_obj) {
   var dates = HTMLschoolDates.replace('%data%',education_obj.schools[item].dates);
   $('.education-entry:last').append(name + degree).append(location).append(dates);
 
+    var i;
     for (i in education_obj.schools[item].majors) {
       var major = HTMLschoolMajor.replace('%data%', education_obj.schools[item].majors[i]);
       $('.education-entry:last').append(major);
@@ -218,6 +226,7 @@ education.onlineClassSectionBuilder  = function (education_obj) {
   if (education_obj.onlineCourses.length > 0) {
     $('#education').append(HTMLonlineClasses);
 
+    var i;
     for (i in education_obj.onlineCourses) {
       $('#education').append(HTMLschoolStart);
       var title = HTMLonlineTitle.replace('%data%', education_obj.onlineCourses[i].name);
@@ -229,8 +238,8 @@ education.onlineClassSectionBuilder  = function (education_obj) {
   }
 };
 
-
 education.onlineClassSectionBuilder(education);
+
 
 // BUILD MAP SECTION
 
@@ -238,13 +247,16 @@ education.onlineClassSectionBuilder(education);
 function locationFinder () {
   var addresses =  [];
     addresses.push(bio.contacts.location);
+    var job_obj;
     for (job_obj in work.jobs) {
       addresses.push(work.jobs[job_obj].location);
     }
+
+    var project_obj;
     for (project_obj in projects.projects) {
       addresses.push(projects.projects[project_obj].location);
     }
-
+   var school_obj;
    for (school_obj in education.schools) {
       addresses.push(education.schools[school_obj].location);
    }
@@ -252,13 +264,14 @@ function locationFinder () {
 }
 
 function mapMaker() {
-  addresses = locationFinder();
+  var addresses = locationFinder();
   var mapOptions = {
     center: { lat: 34.397, lng: -45.644},
     zoom: 3
   };
   var map = new google.maps.Map(document.getElementById('mapDiv'), mapOptions);
-  len = addresses.length;
+  var len = addresses.length;
+  var x;
   for (x = 0; x < len; x++) {
       $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
           var p = data.results[0].geometry.location;
@@ -278,5 +291,3 @@ function mapMaker() {
 $(document).ready(function() {
   mapMaker();
 });
-
-
